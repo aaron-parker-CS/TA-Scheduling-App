1bad0149be6cb8da5c1fb91b751dafb10ff3188b
aaron-parker-CS <parker.w.l.aaron@outlook.com>
2023-11-16 12:32:31 -0600

    Add initial models
    Added initial models and changed settings to link the TAScheduler to the Django project

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from TAScheduler.models import Course
+from TAScheduler.models import Section
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Section)
