cf1c4826d775eb97f900e2f4a7e4978d9185e1b0
Caleb <cewicker@uwm.edu>
2023-11-18 19:44:31 -0600

    Added createCourse.html

--- DjangoUnchained/urls.py
+++ DjangoUnchained/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from DjangoUnchained import settings
-from TAScheduler.views import Home, Dashboard
+from TAScheduler.views import Home, Dashboard, createCourse
 from django.contrib.auth.views import LogoutView
 
 
@@ -27,4 +27,5 @@ urlpatterns = [
     path('', Home.as_view(), name="main-view"),
     path('dashboard/', Dashboard.as_view(), name="dashboard-view"),
     path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
+    path('dashboard/createCourse/', createCourse.as_view(), name="createCourse-view"),
 ]
