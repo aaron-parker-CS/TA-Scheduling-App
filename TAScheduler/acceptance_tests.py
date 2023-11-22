2153517c89ce6fac4508ae85588128107c504e7f
notbil <notbilx@gmail.com>
2023-11-22 07:57:43 -0600

    added acc creation acceptance tests and unit tests for delete

--- TAScheduler/acceptance_tests.py
+++ TAScheduler/acceptance_tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, Client
 from .models import User, UserAssignment, Course, Section, Info
+from django.test import TestCase, Client
+from .models import User, Info
+
 
 class LoginAcceptanceTest(TestCase):
     def setUp(self):
@@ -39,3 +42,89 @@ class LoginAcceptanceTest(TestCase):
         # You may need to adjust the expected message based on your application's validation messages.
         self.assertIn("This field is required.", response.content.decode(),
                       msg="Validation error for empty fields should be displayed.")
+
+
+class AccountCreationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.test_user = User.objects.create_user(username="testuser", email="testuser@example.com",
+                                                  password="testpassword123")
+
+    def test_successful_account_creation(self):
+        # Test creating an account with valid data.
+        response = self.client.post('/create-account/', {
+            "username": "newuser",
+            "password": "newpassword",
+            "email": "newuser@example.com",
+            "phone": "1234567890",
+            "address": "123 Test St",
+            "type": "TA"
+        })
+        self.assertEqual(response.status_code, 200, msg="Failed to create a user account.")
+
+    def test_duplicate_username(self):
+        """
+        Test creating an account with a duplicate username.
+        """
+        User.objects.create_user(username="existinguser", password="password123")
+        response = self.client.post('/create-account/', {
+            "username": "existinguser",  # Duplicate username
+            "password": "newpassword",
+            "email": "newuser@example.com",
+            "phone": "1234567890",
+            "address": "123 Test St",
+            "type": "TA"
+        })
+        self.assertIn("User already exists", response.content.decode(),
+                      msg="Duplicate username should trigger an error message.")
+
+    def test_empty_fields(self):
+        """
+        Test creating an account with empty required fields.
+        """
+        response = self.client.post('/create-account/', {
+            "username": "",
+            "password": "",
+            "email": "",
+            "phone": "",
+            "address": "",
+            "type": ""
+        })
+        self.assertIn("Enter required fields.", response.content.decode(),
+                      msg="Empty required fields should trigger a validation error message.")
+
+    def test_account_creation_with_non_duplicate_non_empty_email(self):
+        response = self.client.post('/create-account/', {
+            "username": "newuser",
+            "password": "newpassword123",
+            "email": "invalidemailformat",
+            "phone": "1234567890",
+            "address": "123 Test St",
+            "type": "TA"
+        })
+        self.assertIn("Creation successful", response.content.decode(),
+                      msg="Account should be created with non-empty, non-duplicate email.")
+
+    def test_account_creation_non_empty_username(self):
+        response = self.client.post('/create-account/', {
+            "username": "",
+            "password": "newpassword123",
+            "email": "newuser@example.com",
+            "phone": "1234567890",
+            "address": "123 Test St",
+            "type": "TA"
+        })
+        self.assertIn("Enter required fields.", response.content.decode(),
+                      msg="Account creation with an empty username should be rejected.")
+
+    def test_account_creation_valid_unique_username(self):
+        response = self.client.post('/create-account/', {
+            "username": "uniqueuser",
+            "password": "newpassword123",
+            "email": "uniqueuser@example.com",
+            "phone": "1234567890",
+            "address": "123 Test St",
+            "type": "TA"
+        })
+        self.assertIn("Creation successful", response.content.decode(),
+                      msg="Account should be created with a unique, non-empty username.")
