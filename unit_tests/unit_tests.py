cc4f4f1cc15574813fa9bf48e963877ca1c464b5
Beckett Frey <83560790+BeckettFrey@users.noreply.github.com>
2023-11-28 23:30:20 -0600

    Update unit_tests.py
    Unit test themselves, still needs some work but mostly done and passing as of this commit.

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -265,3 +265,91 @@ class DeleteCourseTest(TestCase):
         response = self.client.post(self.delete_course_url)
         self.assertFalse(Course.objects.filter(id=self.course.pk).exists(),
                          msg="Course should not exist after deletion.")
+
+class CreateSectionTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.create_section_url = reverse("createSection-view") #ADDED "xx"
+        # create course
+        self.course = Course(course_num=101, semester='Fa', year=2023, credits=3,
+                                            description='Test Course')
+        self.course.save()
+        print(Course.objects.all())
+
+    def test_create_section_success(self):
+
+        response = self.client.post("/createSection/", {
+            'section': 399,
+            'type': "Lab",
+            'course_num':  self.course.__str__(),
+            'start_time': "00:00",
+            'end_time': "00:01",
+            'tuesday': True,
+            'thursday': True,
+            'monday': True,
+            'wednesday': True,
+            'friday': True,
+            'location': "EMS",
+        })
+        print(response.context["message"])
+
+        self.assertEqual(response.status_code, 200, msg="Failed to create a section successfully.")
+
+
+    def test_section_number_validation_low(self):
+        """ Test section number validation (must be between 100 and 999) through POST request """
+        response = self.client.post(self.create_section_url, {
+            'section_num': 100,
+            'course_num': self.course.__str__(),
+            'section_type': "Dis",
+            'section_is_on_monday': True,
+            'section_is_on_wednesday': True,
+        })
+        self.assertNotEqual(response.status_code, 302, msg="Expected a different status code.")
+
+    def test_section_number_validation_high(self):
+        """ Test section number validation for a number too high. """
+        response = self.client.post(self.create_section_url, {
+            'section_num': 1000,
+            'course_num': self.course.__str__(),
+            'section_type': "lecture",
+            'section_is_on_monday': True,
+            'section_is_on_wednesday': True,
+        })
+        self.assertEqual(response.status_code, 200, msg="Failed to handle high section number properly.")
+
+    def test_missing_required_fields_course_num(self):
+        response = self.client.post(self.create_section_url, {
+            'section_num': 402,
+            'section_type': "discussion",
+            'section_is_on_tuesday': True,
+            'section_is_on_thursday': True,
+            # Missing 'course_num''
+        })
+        self.assertNotEqual(response.status_code, 302)
+
+    def test_missing_required_fields_section_num(self):
+        pass
+        
+    def test_duplicate_course_number(self):
+        """ Test that creating a section with a duplicate course number through POST request is handled """
+        # Create a section with course number 101
+        self.client.post(self.create_section_url, {
+            'course_num': self.course.__str__(),
+            'section_num': 101,
+            'section_type': "lecture",
+            'section_is_on_monday': True,
+            'section_is_on_wednesday': True,
+            })
+
+        # Attempt to create another section with the same course number
+        response = self.client.post(self.create_section_url, {
+            'course_num': self.course.__str__(),
+            'section_num': 101,
+            'section_type': "discussion",
+            'section_is_on_thursday': True,
+            'section_is_on_friday': True,
+        })
+
+        self.assertContains(response, "error", msg_prefix="Expected error message in response content for duplicate course number.")
+    
