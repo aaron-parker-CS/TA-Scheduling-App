f83fbda0415f0cedf8ce8a0b196677936048d3d5
Caleb <cewicker@uwm.edu>
2023-12-07 12:21:45 -0600

    Unit tests for dashboard class and functions

--- unit_tests/test_dashboard.py
+++ unit_tests/test_dashboard.py
@@ -5,16 +5,13 @@ from TAScheduler.models import User, UserAssignment, Course, Section, Info, SEME
 from django.db import IntegrityError, DataError
 
 
-class TestDashboard(TestCase):
-    test_user = None
-    test_client = None
-
+class testLoadUsers(TestCase):
     def setUp(self):
-        self.test_client = Client()
-        self.test_user = User(username="test", password="password")
+        pass
 
-    def test_no_login(self):
-        # No login should redirect to the login screen
-        resp = self.test_client.get("/dashboard/")
-        self.assertRedirects(response=resp, expected_url='/', status_code=302,
-                             target_status_code=200, fetch_redirect_response=True)
+    def test_load_users(self):
+        pass
+
+    def test_unsuccessful_load_users(self):
+        pass
+
