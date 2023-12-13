090fffd7745999fe313c78fd5ba8e746d5ab5970
Beckett Frey <beckett.frey@gmail.com>
2023-12-13 01:08:44 -0600

    Update AssignUser class removed unneeded class

--- Classes/AssignUserClass.py
+++ Classes/AssignUserClass.py
@@ -1,4 +1,4 @@
-from TAScheduler.models import UserAssignment
+from TAScheduler.models import UserAssignment, Course
 
 
 def assign_user_to_course(user, course):
@@ -6,6 +6,18 @@ def assign_user_to_course(user, course):
         return False
     try:
         new_assignment = UserAssignment(user_id=user, course=course)
+        new_assignment.save()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def assign_user_to_section(user, section):
+    course = Course.objects.get(id=section.course_id)
+    if UserAssignment.objects.filter(user_id=user, section=section, course=course).exists():
+        return False
+    try:
+        new_assignment = UserAssignment(user_id=user, section=section, course=course)
         new_assignment.save()
         return True
     except Exception as e:
