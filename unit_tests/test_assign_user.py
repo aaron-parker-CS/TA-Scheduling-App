fd1efeedc636e92b87c22fb2642dee74ef996232
aaron-parker-CS <parker.w.l.aaron@outlook.com>
2023-12-14 10:06:58 -0600

    Added more unit tests for get section by course

--- unit_tests/test_assign_user.py
+++ unit_tests/test_assign_user.py
@@ -50,3 +50,25 @@ class TestAssignUser(TestCase):
         self.assertIn(self.test_section, section_list,
                       msg="Get sections by course fails to fetch the section by user assignments")
 
+    def test_get_sections_null(self):
+        with self.assertRaises(ValueError, msg='None argument fails to raise ValueError'):
+            section_list = get_sections_by_course(self.test_user, None)
+            section_list = get_sections_by_course(None, section_list)
+
+    def test_get_sections_invalid(self):
+        with self.assertRaises(ValueError, msg='None argument fails to raise ValueError'):
+            section_list = get_sections_by_course(self.test_user, {})
+            section_list = get_sections_by_course('test', section_list)
+
+    def test_get_sections_other_course(self):
+        new_course = Course(course_num=599, semester='Fa', year=2022, description='Testing')
+        new_course.save()
+        new_section = Section(course=new_course, section_num=400, section_type='Lec', section_is_on_monday=True,
+                              section_is_on_wednesday=True,section_start_time='12:00', section_end_time='13:00',
+                              location='Online')
+        new_section.save()
+        assignment = UserAssignment(user_id=self.test_user, course=self.test_course)
+        assignment.save()
+        section_list = []
+        section_list = get_sections_by_course(self.test_user, section_list)
+        self.assertNotIn(new_section, section_list, msg='Unrelated section shows inside of get sections by course')
